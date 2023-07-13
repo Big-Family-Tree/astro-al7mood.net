dfb19ee0f743369c76e548969d86871e2c565508
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-13 12:09:39 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  site: 'https://big-family-tree.github.io',
+  base: '/astro-al7mood.net',
+
   integrations: [tailwind()]
 });
