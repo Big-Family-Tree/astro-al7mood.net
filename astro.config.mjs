c8cf93870c64df89173a64987f3cacc7d16923c9
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-15 15:38:53 +0300

    al7mood.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://big-family-tree.github.io',
-  base: '/astro-al7mood.net',
+  site: 'https://al7mood.com',
+  base: '/',
 
   integrations: [tailwind()]
 });
